74e34fc82dfe059aea7eb95d1440f75115afdbf7
pfdint <pfdalt@gmail.com>
2014-05-27 22:24:14 -0400

    Some parsing work

--- scripting_system.sh
+++ scripting_system.sh
@@ -14,22 +14,51 @@ set -o errexit
 SS_Extract_Current_Category()
 {
   
-  local 
-  while [[  ]]; do
+  # Rename arguments
+  #
+  local INDEX_BEGIN=$1
+  local INDEX_END=$2
+  local TARGET_INDEX=0
   
+  for ((index=$INDEX_BEGIN; index < $INDEX_END; index++)); do
+    SS_CURRENT_CATEGORY_ITEM_ARRAY[$TARGET_INDEX]=${SS_CATEGORIES_FILE_ARRAY[$index]%%::*}
+    SS_CURRENT_CATEGORY_DESCRIPTION_ARRAY[$TARGET_INDEX]=${SS_CATEGORIES_FILE_ARRAY[$index]##*::}
+    TARGET_INDEX+=1
   done
- 
-  unset<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-##we know the category we want, and we have a global array we can't mutate
+  
+}
+
+SS_Find_Current_Category()
+{
+  
+#we know the category we want, and we have a global array we can't mutate
 #so we have to search the array for the heading we want. can't grep it, have to check
 #every item in the array.
 #When we find it, we start copying it into the result array, until
 #we find the next category heading. 
   
+  # Rename arguments
+  #
+  local CATEGORY_TO_FIND=$1
+  local INDEX_BEGIN=0
+  local INDEX_END=0
+  
+  for ((index=1; index < ${#SS_CATEGORIES_FILE_ARRAY[@]}; index++)); do
+    if [[ ${SS_CATEGORIES_FILE_ARRAY:1} == $CATEGORY_TO_FIND ]]; then
+      INDEX_BEGIN=$index
+    elif [[ ${SS_CATEGORIES_FILE_ARRAY:0:1} == "%" ]]; then
+      INDEX_END=$index
+    fi
+  done
+  
+  # Now that we know our range, we will have another function extract it.
+  #
+  SS_Extract_Current_Category $INDEX_BEGIN $INDEX_END
+  
 }
 
-##prep all of the necessary file by reading them into variables and sterilizing them
-###categories file
+#prep all of the necessary files by reading them into variables and sterilizing them
+##categories file
 SS_CATEGORIES_FILE_RAW="./ss_category_config"
 SS_CATEGORIES_STERILIZED_FILE="./.ss_categories_sterilized"
 
@@ -37,9 +66,14 @@ cat $SS_CATEGORIES_FILE_RAW | sed 's/^#.*//g' | sed '/^$/d' | tee $SS_CATEGORIES
 
 mapfile SS_CATEGORIES_FILE_ARRAY < $SS_CATEGORIES_STERILIZED_FILE
 
+for ((index=1; index < ${#SS_CATEGORIES_FILE_ARRAY[@]}; index++)); do
+  SS_MASTER_ITEMS_ARRAY[$index]=${SS_CATEGORIES_FILE_ARRAY[$index]%%::*}
+  SS_MASTER_DESCRIPTIONS_ARRAY[$index]=${SS_CATEGORIES_FILE_ARRAY[$index]##*::}
+done
+
 #invoke all the necessary functions to display a menu and take input
 
-SS_Extract_Category "TOP LEVEL CATEGORY ONE"
+SS_Find_Current_Category "TOP LEVEL CATEGORY ONE"
 
 #while dislpaying menu
 ##parse category currentcategory
