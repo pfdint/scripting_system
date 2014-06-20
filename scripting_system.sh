546edcacfedfa3fcf5153e19b26885824d0a928b
pfdint <pfdalt@gmail.com>
2014-06-19 23:08:12 -0400

    primordial working parts now

--- scripting_system.sh
+++ scripting_system.sh
@@ -2,97 +2,158 @@
 # scripting_system.sh
 # by pfdint
 # created: 2014-05-18
-# modified: 
+# modified: 2014-06-19
 # purpose: the master ss executable. currently displays menus and stuff.
-
-#we need a config file to know what categories exist.
-#it has to be linear
-#since categories can be nested
 
 set -o errexit
 
-SS_debug()
+# Globals used: $SS_IS_ITEM_CATEGORY
+# $SS_CATEGORY_ARRAY
+# Takes a category name, flips the boolean true if it's a category, false if not.
+#
+SS_Determine_If_Category()
 {
-  echo "==="
-  echo "$1"
-  echo "==="
+    
+    # We assume false for safety.
+    #
+    SS_IS_ITEM_CATEGORY=false
+    
+    local SS_LINE
+    
+    for SS_LINE in ${SS_CATEGORY_ARRAY[@]}; do
+        if [[ $SS_LINE == "%$SS_NEXT_CATEGORY" ]]; then
+            SS_IS_ITEM_CATEGORY=true
+            break
+        fi
+    done
+    
 }
 
-SS_Extract_Current_Category()
+SS_Display_Script()
 {
-  
-  # Rename arguments
-  #
-  local INDEX_BEGIN=$1
-  local INDEX_END=$2
-  local TARGET_INDEX=0
-  
-  for ((index3=$INDEX_BEGIN; index3 < $INDEX_END; index3++)); do
-    SS_CURRENT_CATEGORY_ITEM_ARRAY[$TARGET_INDEX]=${SS_CATEGORIES_FILE_ARRAY[$index3]%%::*}
-    SS_CURRENT_CATEGORY_DESCRIPTION_ARRAY[$TARGET_INDEX]=${SS_CATEGORIES_FILE_ARRAY[$index3]##*::}
-    TARGET_INDEX+=1
-  done
-  
+    echo NYI
 }
 
-SS_Find_Current_Category()
+SS_Display_Item()
 {
-  
-#we know the category we want, and we have a global array we can't mutate
-#so we have to search the array for the heading we want. can't grep it, have to check
-#every item in the array.
-#When we find it, we start copying it into the result array, until
-#we find the next category heading. 
-  
-  # Rename arguments
-  #
-  local CATEGORY_TO_FIND=$1
-  local INDEX_BEGIN=0
-  local INDEX_END=0
-  local BEGIN_FOUND=false
-  
-  for ((index2=1; index2 < ${#SS_CATEGORIES_FILE_ARRAY[@]}; index2++)); do
-    if [[ ${SS_CATEGORIES_FILE_ARRAY[$index2]:1} == $CATEGORY_TO_FIND ]]; then
-      INDEX_BEGIN=$index2
-      BEGIN_FOUND=true
-SS_debug "start category"
-    elif [[ ${SS_CATEGORIES_FILE_ARRAY[$index2]:0:1} == "%" && $BEGIN_FOUND ]]; then
-      INDEX_END=$index2
-SS_debug "category ended"
-      break
+    
+    SS_Determine_If_Category
+    
+    if [[ "$SS_IS_ITEM_CATEGORY" == true ]]; then
+        SS_Display_Category
+    else
+        SS_Display_Script
     fi
-SS_debug "${SS_CATEGORIES_FILE_ARRAY[$index2]:1}"
-  done
-  
-  # Now that we know our range, we will have another function extract it.
-  #
-  SS_Extract_Current_Category $INDEX_BEGIN $INDEX_END
-  
+    
 }
 
-#prep all of the necessary files by reading them into variables and sterilizing them
-##categories file
-SS_CATEGORIES_FILE_RAW="./ss_category_config"
-SS_CATEGORIES_STERILIZED_FILE="./.ss_categories_sterilized"
+SS_Display_Category()
+{
+    
+    local START_LINE=0
+    local END_LINE=${#SS_CATEGORY_ARRAY[@]}
+    local FOUND_NEXT_CATEGORY=false
+    
+    local LINE_INDEX
+    for (( LINE_INDEX=0; LINE_INDEX < ${#SS_CATEGORY_ARRAY[@]}; LINE_INDEX++ )); do
+        if [[ ${SS_CATEGORY_ARRAY[$LINE_INDEX]} =~ ^%$SS_NEXT_CATEGORY$ ]]; then
+            START_LINE=$LINE_INDEX
+            FOUND_NEXT_CATEGORY=true
+        elif [[ "$FOUND_NEXT_CATEGORY" == true ]] && [[ ${SS_CATEGORY_ARRAY[$LINE_INDEX]} =~ ^%.+ ]]; then
+            END_LINE=$LINE_INDEX
+            break
+        fi
+    done
+    
+    START_LINE=$(( $START_LINE + 1 ))
+    local DISTANCE=$(( $END_LINE - $START_LINE ))
+    
+    local CATEGORY_ITEMS=( "${SS_CATEGORY_ARRAY[@]:$START_LINE:$DISTANCE}" )
+    
+    local CATEGORY_ITEM_TITLES=( "${CATEGORY_ITEMS[@]%%::*}" )
+    local CATEGORY_ITEM_DESCRIPTIONS=( "${CATEGORY_ITEMS[@]##*::}" )
+    
+    local CATEGORY_ITEMS_SIZE=${#CATEGORY_ITEMS[@]}
+    local SIZE_NUMBER_LENGTH=${#CATEGORY_ITEMS_SIZE}
+    
+    local TITLE
+    
+    for TITLE in "${CATEGORY_ITEM_TITLES[@]}"; do
+        local TITLE_LENGTHS[${#TITLE}]="$TITLE"
+    done
+    
+    local LONGEST_WORD=${TITLE_LENGTHS[@]: -1}
+    local LONGEST_WORD_SIZE=${#LONGEST_WORD}
+    
+    echo
+    echo
+    echo ===========================================================
+    echo $SS_NEXT_CATEGORY
+    echo ===========================================================
+    echo
+    
+    local PERMANENT_PADDING=".........."
+    
+    local ITEM_NUMBER
+    
+    for ITEM_NUMBER in ${!CATEGORY_ITEMS[@]}; do
+        local IDENTIFICATION_NUMBER=$(( $ITEM_NUMBER + 1 ))
+        local ITEM_TITLE=${CATEGORY_ITEM_TITLES[$ITEM_NUMBER]}
+        
+        unset IDENTIFIER
+        
+        local ITERATION
+        for (( ITERATION=0; ITERATION < $SIZE_NUMBER_LENGTH - ${#IDENTIFICATION_NUMBER}; ITERATION++ )); do
+            IDENTIFIER="$IDENTIFIER "
+        done
+        
+        IDENTIFIER="$IDENTIFICATION_NUMBER$IDENTIFIER - "
+        
+        local PADDING_SIZE=$(( ${LONGEST_WORD_SIZE} - ${#ITEM_TITLE} ))
+        
+        unset PADDING
+        
+        for (( ITERATION=0; ITERATION < $PADDING_SIZE; ITERATION++ )); do
+            PADDING="${PADDING}."
+        done
+        
+        local INDENT="    "
 
-cat $SS_CATEGORIES_FILE_RAW | sed 's/^#.*//g' | sed '/^$/d' | tee $SS_CATEGORIES_STERILIZED_FILE
+        echo "$INDENT$IDENTIFIER${CATEGORY_ITEM_TITLES[$ITEM_NUMBER]} $PADDING$PERMANENT_PADDING ${CATEGORY_ITEM_DESCRIPTIONS[$ITEM_NUMBER]}"
+    done
+    
+    echo "$INDENT----"
+    echo "${INDENT}p  - return to previous menu"
+    echo "${INDENT}q  - quit"
+    echo
+    
+    read -p "Select an option: " SELECTION
+    
+    #PURPOSEFUL STRING COMPARISON; NOT INTEGER COMPARISON
+    if [[ "$SELECTION" == "0" ]]; then
+        SS_NEXT_CATEGORY=$ITEM_TITLE
+        echo will redisplay ourselves, do not push to stack
+    elif [[ $SELECTION =~ ^[[:digit:]]+$ ]]; then
+        if [[ $SELECTION -le ${#CATEGORY_ITEMS[@]} ]]; then
+            echo ${CATEGORY_ITEM_TITLES[$SELECTION - 1]} will be the next menu
+        else
+            echo not a valid selection for this menu
+        fi
+        elif [[ "$SELECTION" =~ %[pq]$ ]]; then
+            case "$SELECTION" in
+                p)
+                    echo p was selected
+                    ;;
+                q)
+                    echo q was selected
+                    ;;
+            esac
+        else
+            echo that was not valid input and you know it
+        fi
+    
+}
 
-mapfile SS_CATEGORIES_FILE_ARRAY < $SS_CATEGORIES_STERILIZED_FILE
-
-for ((index1=1; index1 < ${#SS_CATEGORIES_FILE_ARRAY[@]}; index1++)); do
-  SS_MASTER_ITEMS_ARRAY[$index1]=${SS_CATEGORIES_FILE_ARRAY[$index1]%%::*}
-  SS_MASTER_DESCRIPTIONS_ARRAY[$index1]=${SS_CATEGORIES_FILE_ARRAY[$index1]##*::}
-done
-
-#invoke all the necessary functions to display a menu and take input
-
-SS_Find_Current_Category "TOP LEVEL CATEGORY TWO"
-
-echo "---"
-echo ${SS_CURRENT_CATEGORY_ITEM_ARRAY[@]}
-
-#while dislpaying menu
-##parse category currentcategory
-##display the information we found
-##wait for input from the user
-##parse it and change variables accordingly
+mapfile -t SS_CATEGORY_ARRAY < categories.config
+SS_NEXT_CATEGORY=$1
+SS_Display_Item "$1"
